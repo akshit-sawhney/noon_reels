e6eb4cedc6d9cd6837deb2d5d39b598d198967e8
akshit-non <akshit@non.sa>
2021-07-17 01:43:24 +0530

    Exposes endpoint to update views

--- routes/likes.js
+++ routes/likes.js
@@ -6,11 +6,18 @@ const
     express = require('express')
     , router = express.Router()
     ;
-const { updateLikesByReelId } = require('../controllers/noon_reels');
+const { updateLikesByReelId, updateViewsByReelId } = require('../controllers/noon_reels');
 router.post('/update_likes', async (req, res, next) => {
     const updateResponse  = updateLikesByReelId(req.body.reel_id, req.body.likes);
     return res.status(200).send({
         message: "Likes Updated"
+    });
+});
+
+router.post('/update_views', async (req, res, next) => {
+    const updateResponse  = updateViewsByReelId(req.body.reel_id, req.body.views);
+    return res.status(200).send({
+        message: "Views Updated"
     });
 });
 
